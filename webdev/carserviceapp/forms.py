116a957442a35d5cf123ce60bd3f63e6c97751b9
Manasi <m.varaiya@somaiya.edu>
2021-03-27 13:45:20 +0530

    navbar updates

--- webdev/carserviceapp/forms.py
+++ webdev/carserviceapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-from carserviceapp.models import User
+from carserviceapp.models import User,Shop
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(help_text='Required')
@@ -9,3 +9,8 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['email','first_name','last_name','phone_number','password1','password2']
+
+class ShopForm(forms.ModelForm):
+    class Meta:
+        model = Shop
+        fields = ['shop_name','owner_name','shop_email','shop_contact','owner_contact','shop_location','services','shop_image','service_charge']
--- webdev/carserviceapp/views.py
+++ webdev/carserviceapp/views.py
@@ -1,7 +1,7 @@
 import os
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,authenticate
-from carserviceapp.forms import RegistrationForm
+from carserviceapp.forms import RegistrationForm, ShopForm
 # Create your views here.
 def home_page(request):
     return render(request, 'homepage.html')
@@ -21,3 +21,13 @@ def registration_view(request):
         form = RegistrationForm()
         context['registration_form'] = form
     return render(request,'registration/register.html',context)
+
+def shop_view(request):
+    form = ShopForm()
+    context={}
+    if request.POST:
+        form = ShopForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, 'registration/shopform.html', {'form': form})
