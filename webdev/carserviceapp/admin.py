5928e21f0c71dea1b88daed7a2fd21583c6fd580
Manasi <m.varaiya@somaiya.edu>
2021-03-27 14:31:24 +0530

    booking ui done

--- webdev/carserviceapp/admin.py
+++ webdev/carserviceapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from carserviceapp.models import User,Shop
+from carserviceapp.models import User,Shop,Booking
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Shop)
+admin.site.register(Booking)
